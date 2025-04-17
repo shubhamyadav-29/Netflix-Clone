7b3160bbe826e8b8aca1d943b9e9cc8bdd93d8e0
shubhamyadav-29 <shubhamarjun5@gmail.com>
2025-04-17 21:22:05 +0530

    again made the changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace with your repo name
-const repoName = 'your-repo-name'
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // Replace with your repo name
+// const repoName = 'your-repo-name'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: `/${repoName}/`,
+// })
 
