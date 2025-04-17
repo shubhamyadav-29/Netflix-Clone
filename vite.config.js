8d3371100be51d4957879a2e2930c8b54a13ab94
shubhamyadav-29 <shubhamarjun5@gmail.com>
2025-04-17 21:39:48 +0530

    oky

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix-Clone/', // Add this line to specify your GitHub repo name
 })
+
 
