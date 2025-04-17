a9c7ab1ccf9acc538308073982c632caace4ebb9
shubhamyadav-29 <shubhamarjun5@gmail.com>
2025-04-17 21:29:08 +0530

    again trying

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Netflix-Clone/', // Add this line to specify your GitHub repo name
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // Replace with your repo name
-// const repoName = 'your-repo-name'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: `/${repoName}/`,
-// })
 
